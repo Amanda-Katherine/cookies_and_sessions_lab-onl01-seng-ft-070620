e1b6501f2f2d355cd8eaaf50ab703a25ed82c9e2
Amanda_Katherine <walker.akw@gmail.com>
2020-09-23 01:44:14 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get '/products', to: 'products#index'
+  # get '/products', to: 'products#index'
   
-  # root 'products#index'
+  root 'products#index'
   post '/products', to: 'products#add'
   
  
